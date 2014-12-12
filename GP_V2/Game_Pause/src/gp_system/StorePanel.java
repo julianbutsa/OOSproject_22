c61cec7fadaea82c747044c5a234d957862f8679
SanchezAlfredo <taioblack@gmail.com>
2014-12-12 14:49:00 -0600

    merge

--- GP_V2/Game_Pause/src/gp_system/StorePanel.java
+++ GP_V2/Game_Pause/src/gp_system/StorePanel.java
@@ -8,38 +8,23 @@ import java.util.Iterator;
 import javax.swing.JPanel;
 
 import containers.Item;
+import controllers.Store;
 import db_system.DBItem;
 
 public class StorePanel extends JPanel{
+	private Store myStore;
 	private ArrayList<Item> items;
 	private ArrayList<ItemPanel> itemPanels;
 	
 	public StorePanel(){
-		DBItem d = new DBItem();
-		try {
-			d.connect();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		myStore = new Store();
+		myStore.getItems(items);
+		
 		this.setLayout(new GridLayout( 4,2));
 		
 		this.items = new ArrayList<Item>();
 		this.itemPanels = new ArrayList<ItemPanel>();
 		
-		if(d.itemSearch(items)){
-			
-			System.out.println("yay");
-		}else{
-			throw new RuntimeException ("failed to get items");
-		}
-		/*
-		d.itemSearch(items);
-		for(int i =0; i<items.size(); i++){
-			System.out.println(items.get(i));
-			
-		}
-		*/
 		Iterator<Item> i = items.iterator();
 		while(i.hasNext()){
 			ItemPanel temp = new ItemPanel(i.next());
